9167c0a18558a4562ed2d631eebba40da1977ce4
24shrouk <shroukhegab3@gmail.com>
2025-10-13 21:54:22 +0300

    add all colors of app

---
+++ lib/core/constants/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColor {
+  static const Color primaryColor = Color(0xFF44BDB6);
+  static const Color black909Color = Color(0xFF090909);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greyF9FColor = Color(0xFF9F9F9F);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color greyF8FColor = Color(0xFF8F8F8F);
+  static const Color green9F7Color = Color(0xFFEDF9F7);
+  static const Color grey464Color = Color(0xff646464);
+  static const Color greyC8CColor = Color(0xff8C8C8C);
+  static const Color grey298Color = Color(0xff8B9298);
+}
