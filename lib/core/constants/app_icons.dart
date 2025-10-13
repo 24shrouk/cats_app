0492ad6db434023c7637c002bc9b7dadc868fe0d
24shrouk <shroukhegab3@gmail.com>
2025-10-13 22:05:10 +0300

    add images and icons

---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,11 @@
+abstract class AppIcons {
+  static const String fiterationIcon = 'assets/icons/filteration_icon.svg';
+  static const String getStartedIcon = 'assets/icons/get_started_icon.svg';
+  static const String heartIcon = 'assets/icons/heart_icon.svg';
+  static const String homeIcon = 'assets/icons/home_icon.svg';
+  static const String locationIcon = 'assets/icons/location_icon.svg';
+  static const String messagesIcon = 'assets/icons/messages_icon.svg';
+  static const String notificationIcon = 'assets/icons/notification.svg';
+  static const String profileIcon = 'assets/icons/profile_icon.svg';
+  static const String searchIcon = 'assets/icons/search_normal_icon.svg';
+}
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,5 @@
+abstract class AppImages {
+  static const String animalImage = 'assets/images/animal_images.png';
+  static const String dogAndCatImage = 'assets/images/dog_and_cat_image.png';
+  static const String heartImage = 'assets/images/heart_image.png';
+}
